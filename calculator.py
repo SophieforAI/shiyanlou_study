d035cd7549a5302ba78a1ca56f01ade414185a50
sophie19921005 <m18768120089@163.com>
2018-08-04 12:32:41 +0800

    fix file calculator.py

--- calculator.py
+++ calculator.py
@@ -1,41 +1,58 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 import sys 
 import csv
 
 
-class cfg_input:
-    def __init__(self,filename):
-        self._filename = filename
-        
-    def writecfg(self):
-        with open(self._filename,'a') as file:
-            file.write('JiShuL = 2193.00')
-            file.write('JiShuH = 16446.00')
-            file.write('YangLao = 0.08')
-            file.write('YiLiao = 0.02')
-            file.write('ShiYe = 0.005')
-            file.write('GongShang = 0')
-            file.write('ShengYu = 0')
-            file.write('GongJiJin = 0.06')
-
-
-
-
-
-class csv_input:
-    def __init__(pass):
-        pass
-    def readcsv(self):
-        return
-
-
-
-class Calculator:
+class Args(object):
     def __init__(self):
-        pass
-    def tax_calculator(self):
-        return (id,sqgz,sbje,gsje,shgz)
+        self.args = sys.argv[1:]
+        
+    def read_fileroad(self):
+        index0 = self.args.index('-c')+1
+        index1 = self.args.index('-d')+1
+        index2 = self.args.index('-o')+1
+        return([self.args[index0],self.args[index1],self.args[index2]])
+        
+            
+class Config(object):
+    def __init__(self,cfgfile):
+        self.config = self._read_config(cfgfile)
+    def _read_config(self,cfgfile):
+        config = {}
+        with open(cfgfile,'r') as file:
+            for line in file:
+                l = line.strip().split('=')
+                config[l[0].strip()]=l[1].strip()
+        print(config)
+        return config
+        
+        
+        
 
-if __name__ ='__main__':
-    cfg_write = cfg_input('/home/shiyanlou/test.cfg')
-    cfg_write.writecfg()
+
+class UserData(object):
+    def __init__(self):
+        self.userdata = self._read_users_data()
+    def _read_users_data(self):
+        userdata =[]
+
+class IncomeTaxCalculator(object):
+    def calc_for_all_userdata(self):
+        pass
+    def export(self,default = 'csv'):
+        result = self.calc_for_all_userdata()
+        with open() as f :
+           
+            write = csv.write(f)
+            write.writerows(result)
+
+  
+
+if __name__ =='__main__':
+    Arg = Args()
+    file_road = Arg.read_fileroad()
+    cfg_file = file_road[0]
+    print(cfg_file)
+    Config = Config(cfg_file)
+   # print(Config._read_config(cfg_file))
+    
