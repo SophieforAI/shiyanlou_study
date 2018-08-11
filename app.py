1bd4e74e83ee04688617e9d205c8318dcb821bb5
sophie19921005 <m18768120089@163.com>
2018-08-11 18:09:54 +0800

    second week chanllege

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-
 from flask import Flask,render_template
-app = Flask(__name__)
+import os
+import json
+
+app =Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD']=True
+
+os.chdir('/home/shiyanlou/files/')
 
 @app.route('/')
 def index():
-    teacher = {
-                'name': 'Aiden',
-                'email': 'luojin@simplecloud.cn'
-                                }
+    file_list =os.listdir('/home/shiyanlou/files/')
+    title_list = []
+    for js in file_list:
+        with open(js,'r') as f:
+            json_content = json.loads(f.read())
+            json_title=json_content['title']
+            title_list.append(json_title)
+    return render_template('index.html',title_list=title_list)
 
-    course = {
-                'name': 'Python Basic',
-                'teacher': teacher,
-                'user_count': 5348,
-                'price': 199.0,
-                'lab': None,
-                'is_private': False,
-                'is_member_course': True,
-                'tags': ['python', 'big data', 'Linux']
-    }
-    return render_template('index.html',course=course)
+
+
+@app.errorhandler(404)
+@app.route('/files/<filename>')
+def file(filename):
+     
+    js = '/home/shiyanlou/files/{}.json'.format(filename)
+    if not os.path.exists(js):
+        return render_template('404.html'),404
+    else:
+        with open(js,'r') as f:
+            json_content = json.loads(f.read())
+            json_con = json_content['content']
+        return json_con
+
+
+#@app.errorhandler(404)
+#def not_found(filename):
+    #js = '/home/shiyanlou/files/{}.json'.format(filename)
+    #if not os.path.exists(js):
+        #return render_template('404.html'),404
+
+if __name__=='__main__':
+    app.run()
