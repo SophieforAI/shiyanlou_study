74592a41877243c35a65f7f18e90ee90f33c4173
sophie19921005 <m18768120089@163.com>
2018-08-13 23:48:43 +0800

    challenge

--- second_week/app.py
+++ second_week/app.py
@@ -1,44 +1,61 @@
 from flask import Flask,render_template
 import os
 import json
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey,create_engine
+
+engine = create_engine('mysql://root:@localhost/challenge')
+
+
+
+from datetime import datetime
 
 app =Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD']=True
-
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root@localhost/challenge'
 os.chdir('/home/shiyanlou/files/')
+db = SQLAlchemy(app)
+
+class File(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(80))
+    created_time = db.Column(db.DateTime)
+    category_id = db.Column(db.Integer,db.ForeignKey('category.id'))
+    content =db.Column(db.Text)
+    category = db.relationship('Category',backref=db.backref('files',lazy='dynamic'))
+    def __init__(self,title,created_time,category,content):
+        self.title = title
+        self.created_time = created_time
+        self.category = category
+        self.content = content
+    def __repr__(self):
+        return '<File %r>' %self.title
+
+class Category(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(80))
+    def __init__(self,name):
+        self.name = name
+    def __repr__(self):
+        return '<Category %r>'% self.name
 
 @app.route('/')
 def index():
-    file_list =os.listdir('/home/shiyanlou/files/')
-    title_list = []
-    for js in file_list:
-        with open(js,'r') as f:
-            json_content = json.loads(f.read())
-            json_title=json_content['title']
-            title_list.append(json_title)
-    return render_template('index.html',title_list=title_list)
+    files = File.query.all()
+    return render_template('index.html',files=files)
 
 
 
 @app.errorhandler(404)
-@app.route('/files/<filename>')
-def file(filename):
-     
-    js = '/home/shiyanlou/files/{}.json'.format(filename)
-    if not os.path.exists(js):
+@app.route('/files/<file_id>')
+def file(file_id):
+    sql = 'select * from file where id = {}'.format(file_id)
+        
+    file_content = engine.execute(sql).fetchall()
+    if file_content==[]:
         return render_template('404.html'),404
     else:
-        with open(js,'r') as f:
-            json_content = json.loads(f.read())
-            json_con = json_content['content']
-        return json_con
-
-
-#@app.errorhandler(404)
-#def not_found(filename):
-    #js = '/home/shiyanlou/files/{}.json'.format(filename)
-    #if not os.path.exists(js):
-        #return render_template('404.html'),404
+        return render_template("file.html", file_item = file_content) 
 
 if __name__=='__main__':
     app.run()
